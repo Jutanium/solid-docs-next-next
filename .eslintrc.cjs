ee52e41bd638693bceb3f70b12a2b5d911dee2fb
Dan Jutan <danjutan@gmail.com>
2023-04-02 16:05:21 -0400

    set up eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ["@typescript-eslint", "astro", "jsx-a11y", "solid"],
+	root: true,
+	overrides: [
+		{
+			// Define the configuration for `.astro` file.
+			files: ["*.astro"],
+			// Allows Astro components to be parsed.
+			parser: "astro-eslint-parser",
+			// Parse the script in `.astro` as TypeScript by adding the following configuration.
+			// It's the setting you need when using TypeScript.
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+				extraFileExtensions: [".astro"],
+			},
+			rules: {},
+		},
+	],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"plugin:astro/recommended",
+		"plugin:solid/typescript",
+	],
+	rules: {
+		"no-undef": "off",
+	},
+};
