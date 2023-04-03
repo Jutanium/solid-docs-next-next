a6b610bc013011ff70de1aeb422808c2b80acefb
Dan Jutan <danjutan@gmail.com>
2023-04-02 21:35:12 -0400

    theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
+import Icons from "unplugin-icons/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+	integrations: [solidJs(), mdx()],
+	vite: {
+		plugins: [
+			Icons({
+				compiler: "solid",
+			}),
+		],
+	},
 });
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,44 +1,38 @@
-import { Component, createEffect, createSignal, onMount } from 'solid-js';
-import CarbonSun from '~icons/carbon/sun';
-import CarbonMoon from '~icons/carbon/moon';
-import { ToggleButton } from '@kobalte/core';
+import { Component, createEffect, createSignal, onMount } from "solid-js";
+import CarbonSun from "~icons/carbon/sun";
+import CarbonMoon from "~icons/carbon/moon";
+import { ToggleButton } from "@kobalte/core";
 
 const ThemeToggle: Component = () => {
-  const [isDark, setIsDark] = createSignal<boolean>(false);
+	const [isDark, setIsDark] = createSignal<boolean>(false);
 
-  onMount(() => {
-    setIsDark(localStorage.getItem('theme') === 'dark');
-  });
+	onMount(() => {
+		setIsDark(localStorage.getItem("theme") === "dark");
+	});
 
-  createEffect(() => {
-    if (isDark()) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-  });
+	createEffect(() => {
+		if (isDark()) {
+			document.documentElement.setAttribute("data-theme", "dark");
+		} else {
+			document.documentElement.setAttribute("data-theme", "light");
+		}
+	});
 
-  function toggleTheme() {
-    setIsDark(!isDark());
-    localStorage.setItem('theme', isDark() ? 'dark' : 'light');
-  }
+	function toggleTheme() {
+		setIsDark(!isDark());
+		localStorage.setItem("theme", isDark() ? "dark" : "light");
+	}
 
-  return (
-    <ToggleButton.Root
-      isPressed={isDark()}
-      onPressedChange={toggleTheme}
-      class="h-8 w-8"
-      aria-label="Mute"
-    >
-      {(state) =>
-        state.isPressed() ? (
-          <CarbonMoon class="h-full w-full" />
-        ) : (
-          <CarbonSun class="h-full w-full" />
-        )
-      }
-    </ToggleButton.Root>
-  );
+	return (
+		<ToggleButton.Root
+			isPressed={isDark()}
+			onPressedChange={toggleTheme}
+			class="h-8 w-8"
+			aria-label="Mute"
+		>
+			{(state) => (state.isPressed() ? <CarbonMoon /> : <CarbonSun />)}
+		</ToggleButton.Root>
+	);
 };
 
 export default ThemeToggle;
