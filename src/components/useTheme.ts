b51b849f2a305e41e854f2e53256cae4b4134144
Dan Jutan <danjutan@gmail.com>
2023-04-02 21:18:16 -0400

    colors and fonts

---
+++ src/components/useTheme.ts
@@ -0,0 +1,22 @@
+import { createMutationObserver } from "@solid-primitives/mutation-observer";
+import { createMemo, createSignal } from "solid-js";
+
+// https://github.com/codemirror/theme-one-dark/blob/main/src/one-dark.ts
+
+const [isDark, setIsDark] = createSignal(false);
+
+createMutationObserver(
+	() => document.documentElement,
+	{ attributes: true },
+	(mutations) => {
+		mutations.forEach((mutation) => {
+			if (mutation.attributeName === "data-theme") {
+				setIsDark(
+					document.documentElement.getAttribute("data-theme") === "dark"
+				);
+			}
+		});
+	}
+);
+
+export { isDark };
